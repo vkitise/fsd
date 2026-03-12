2d2442631106a0fe9300bc71f066227a3d6e0123
vkitise <reddyreddy04sd@gmail.com>
2026-03-12 10:51:27 +0530

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,32 +1,21 @@
-const readline = require('readline');
-
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-function isPalindrome(str) {
-  const reversedStr = str.split('').reverse().join('');
-  return str === reversedStr;
-}
-
-rl.question('Enter a string: ', (inputString) => {
-  const lengthOfString = inputString.length;
-  console.log(`Length of the string: ${lengthOfString}`);
-
-  const start = inputString.indexOf("JavaScript");
-  if (start !== -1) {
-    const extractedWord = inputString.slice(start, start + "JavaScript".length);
-    console.log(`Extracted word: ${extractedWord}`);
+rl.question("Enter a string: ", function(str) {
+  console.log("Length of the string:", str.length);
+  if (str.includes("JavaScript")) {
+    console.log("Extracted word: JavaScript");
   } else {
     console.log("Word 'JavaScript' not found in the string.");
   }
-
-  const replacedString = inputString.replace('JavaScript', 'JavaScript lab program');
-  console.log(`New string after replacement: ${replacedString}`);
-
-  const isStringPalindrome = isPalindrome(inputString);
-  console.log(`Is the string a palindrome? ${isStringPalindrome}`);
-
+  let newStr = str.replace("JavaScript","JavaScript lab program");
+  console.log("New string after replacement:", newStr);
+  let rev = str.split("").reverse().join("");
+  if (str === rev)
+    console.log("Is the string a palindrome? true");
+  else
+    console.log("Is the string a palindrome? false");
   rl.close();
 });
